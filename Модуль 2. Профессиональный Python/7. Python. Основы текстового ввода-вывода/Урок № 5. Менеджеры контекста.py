265fa4e7121bb514ee6277ca6d9d6a930b7349db
Andradit <drakushev@mail.ru>
2024-02-03 13:10:10 +0300

    add changes, 03.02.2024

--- Модуль 2. Профессиональный Python/7. Python. Основы текстового ввода-вывода/Урок № 5. Менеджеры контекста.py
+++ Модуль 2. Профессиональный Python/7. Python. Основы текстового ввода-вывода/Урок № 5. Менеджеры контекста.py
@@ -61,103 +61,56 @@ file_1 = open('file1.txt', 'r')
 file_2 = open('file2.txt', 'r')
 output = open('out.txt', 'w')
 
-import itertools
-from itertools import zip_longest
-
-import shutil
-
-
-# file_1.close()
-# file_2.close()
 
 def merge(file1, file2, out):
-    with (open(file1, 'r') as f_1, open(file2, 'r') as f_2):
-        with open(out, 'w') as o:
+    with (open(file1, 'r') as input_file_1, open(file2, 'r') as input_file_2):
+        with open(out, 'w') as output_file:
             diff_1 = []
             diff_2 = []
-            for line_1, line_2 in zip_longest(f_1, f_2):
-                print(f'{diff_1=}, {diff_2=}, {line_1=}, {line_2=}')
+            for line_1, line_2 in zip(input_file_1, input_file_2):
+                # print(f'{diff_1=}, {diff_2=}, {line_1=}, {line_2=}')
                 if line_1 != line_2:
                     diff_1.append(line_1)
                     diff_2.append(line_2)
                 else:
                     if diff_1 and diff_2:
-                        o.write(f'>>>file1>>>\n{"".join(diff_1)}=====\n{"".join(diff_2)}<<<file2<<<\n')
+                        output_file.write(
+                            f'>>>file1>>>\n{"".join(diff_1)}=====\n'
+                            f'{"".join(diff_2)}<<<file2<<<\n')
                         diff_1 = []
                         diff_2 = []
-                    o.write(line_1)
+                    output_file.write(line_1)
             if diff_1 and diff_2:
-                o.write(f'>>>file1>>>\n{"".join(diff_1)}=====\n{"".join(diff_2)}<<<file2<<<\n')
+                output_file.write(f'>>>file1>>>\n{"".join(diff_1)}=====\n'
+                                  f'{"".join(diff_2)}<<<file2<<<\n')
 
 
+'SOLUTION'
 
-
-    # open(out, 'w').writelines(strings_of_file1)
-    # print(strings_of_file1)
-    # print(strings_of_file2)
-    # a = ''
-    # b = ''
-    # c = ''
-    # d = ''
-    # res = (f'{a}>>>file1>>>\n{b}=====\n'
-    #        f'{c}<<<file2<<<{d}')
-    # # res_1 = []
-    # # res_2 = []
-    # # res_3 = []
-    # for index, line_1 in enumerate(strings_of_file1):
-    #     # if strings_of_file2[index] == line_1:
-    #     #     d += strings_of_file2[index]
-    #     if line_1 != strings_of_file2[index]:
-    #         b += line_1
-    #         c += strings_of_file2[index]
-    # if line_1 == strings_of_file2[index]:
-    #     # res_1.append(line_1)
-    #     a += line_1
-    #     d += line_1
-
-    # return f'{a}\n>>>file1>>>\n{b}=====\n{c}<<<file2<<<\n{d}'
-
-    # res_1.extend(res_2)
-    # res = [x for x in itertools.chain(res_1, res_2, res_3)]
-    # print(res)
-
-    # print(f'>>>file1>>>\n{line_1}=====\n'
-    #       f'{strings_of_file2[index]}<<<file2<<<')
-    # res = ['>>>file1>>>\n']
-    # for index_1, line_1 in enumerate(strings_of_file1):
-    #     if line_1 in strings_of_file2:
-    #         res.insert(index_1, line_1)
-    #
-    #     if line_1 not in strings_of_file2:
-    #
-    #         res.append(line_1)
-    #
-    #     res.append(line_1)
-
-    # res += '>>>file1>>>\n'
-    # elif line_1 not in strings_of_file2:
-    #     res += line_1
-    # for line_2 in strings_of_file2:
-    #     if line_2 in strings_of_file1:
-    #         res += line_2
-    #         res += '<<<file2<<<\n' + '=====\n'
-    #     if line_2 not in strings_of_file1:
-    #         res += line_2
-
-    # for line_2 in strings_of_file2:
-    # else:
-    #     res += '>>>file1>>>\n' + '====='
-    # return res
-
-    # print(res)
-    # if line_2 not in strings_of_file1:
-    #     res += line_2
-    #     res += '<<<file2<<<'
-
-    # print(strings_of_file1)
-    # print(strings_of_file2)
-    # result_1 = open(out, 'w').writelines(res)
-    # return result_1
-
+# def merge(file1, file2, out):
+#     with (
+#         open(file1, 'r') as f1,
+#         open(file2, 'r') as f2,
+#         open(out, 'w') as fout,
+#     ):
+#         diff1 = []
+#         diff2 = []
+#         for line1, line2 in zip(f1, f2):
+#             if line1 == line2:
+#                 if diff1 or diff2:
+#                     fout.write(make_diff_view(diff1, diff2))
+#                     diff1 = []
+#                     diff2 = []
+#                 fout.write(line1)
+#             else:
+#                 diff1.append(line1)
+#                 diff2.append(line2)
+#         if diff1 or diff2:
+#             fout.write(make_diff_view(diff1, diff2))
+#
+#
+# def make_diff_view(diff1, diff2):
+#     template = f">>>file1>>>\n{''.join(diff1)}=====\n{''.join(diff2)}<<<file2<<<\n"  # noqa: E501
+#     return template
 
 print(merge('file1.txt', 'file2.txt', 'out.txt'))
